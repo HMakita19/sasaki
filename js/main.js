f57c1b1366785be759a4c2220dc4de3209835cf2
HMakita19 <mracleofendymion@icloud.com>
2022-09-05 11:31:57 +0900

    各画面　header footer　共通化

--- js/main.js
+++ js/main.js
@@ -1,19 +1,46 @@
 // ヘッダー呼び出し
-function include_header(){
+// function include_header(){
+//   $.ajax({
+//       url: '/include/header.html', // リクエストを送信するURLを指定
+//       async: false, // 非同期リクエストを無効にする
+//   }).done(function(header_html){ // 通信が成功したら
+//       document.write(header_html); // herder.htmlを表示する
+//   });
+// }
+
+// フッター呼び出し
+// function include_footer(){
+//   $.ajax({
+//       url: '/include/footer.html', // リクエストを送信するURLを指定
+//       async: false, // 非同期リクエストを無効にする
+//   }).done(function(footer_html){ // 通信が成功したら
+//       document.write(footer_html); // herder.htmlを表示する
+//   });
+// }
+
+
+
+// header
+function include_header(rootDir){
   $.ajax({
-      url: '/include/header.html', // リクエストを送信するURLを指定
+      url: rootDir + 'include/header.html', // リクエストを送信するURLを指定
       async: false, // 非同期リクエストを無効にする
   }).done(function(header_html){ // 通信が成功したら
+      header_html = header_html.replace(/\{root\}/g, rootDir); // header.htmlの文字列を置き換え
+
       document.write(header_html); // herder.htmlを表示する
   });
 }
 
-// フッター呼び出し
-function include_footer(){
+
+// footer
+function include_footer(rootDir){
   $.ajax({
-      url: '/include/footer.html', // リクエストを送信するURLを指定
+      url: rootDir + 'include/footer.html', // リクエストを送信するURLを指定
       async: false, // 非同期リクエストを無効にする
   }).done(function(footer_html){ // 通信が成功したら
-      document.write(footer_html); // herder.htmlを表示する
+      footer_html = footer_html.replace(/\{root\}/g, rootDir); // footer.htmlの文字列を置き換え
+
+      document.write(footer_html); // footer.htmlを表示する
   });
 }
